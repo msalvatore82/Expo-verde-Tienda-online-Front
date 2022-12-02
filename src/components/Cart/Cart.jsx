b3840ddc62b6be6ad6e564a566c14b5cd512f99e
matias salvatore <[EMAIL]>
2022-12-03 00:19:28 +0100

    cart products

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,40 +1,63 @@
 import React, { useContext, useEffect } from "react";
-import { Divider, List, Typography ,Button} from "antd";
+import { Divider, Button, Card } from "antd";
 import { ShoppingOutlined } from "@ant-design/icons";
 import { ProductsContext } from "../../context/ProductContext/ProductState";
 import { OrderContext } from "../../context/OrderContext/OrderState";
 
 const Cart = () => {
   const { cart, clearCart } = useContext(ProductsContext);
-  const {createOrder} = useContext(OrderContext)
-  const cartNames = cart.map((cartItem) => cartItem.name);
-const createNewOrder =()=>{
-    createOrder(cart)
-    clearCart()
-}
+  const { createOrder } = useContext(OrderContext);
+  const createNewOrder = () => {
+    createOrder(cart);
+    clearCart();
+  };
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cart));
   }, [cart]);
 
   return (
-    <div>
-      <Divider orientation="left">Your Cart</Divider>
-      <List
-        header={<div>Products Selected</div>}
-        footer={
-          <div>
-            <Button onClick={clearCart}>Clear Cart</Button>
-            <Button onClick={createNewOrder}>Buy <ShoppingOutlined /></Button>
-          </div>
-        }
-        bordered
-        dataSource={cartNames}
-        renderItem={(item) => (
-          <List.Item>
-            <Typography.Text mark></Typography.Text> {item}
-          </List.Item>
-        )}
-      />
+    <div
+      style={{
+        width: 700,
+        border: "1px solid gray",
+        borderRadius: 10,
+      }}
+    >
+      <div className="container-products">
+        <Divider orientation="left">Your Cart</Divider>
+        {cart.map((cartItem) => {
+          return (
+            <div key={cartItem.id} className="site-card-border-less-wrapper">
+              <Card
+                title={cartItem.name}
+                bordered={true}
+                style={{
+                  width: 300,
+                  border: "1px solid black",
+                  textAlign: "center",
+                }}
+              >
+                <img
+                  src={cartItem.image}
+                  alt=""
+                  style={{
+                    width: 150,
+                    border: "1px solid gray",
+                    borderRadius: 10,
+                  }}
+                />
+                <p>{cartItem.price} €</p>
+              </Card>
+            </div>
+          );
+        })}
+      </div>
+      <div>
+        <Button onClick={clearCart}>Clear Cart</Button>
+        <Button onClick={createNewOrder}>
+          Buy <ShoppingOutlined />
+        </Button>
+      </div>
     </div>
   );
 };
