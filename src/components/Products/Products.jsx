c59205955ff81f69dfb23b233a143ac75dd79f41
diegopalones <diego.palones@gmail.com>
2022-12-09 12:58:01 +0100

    merging

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,8 +1,9 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Card, Button, Row } from "antd";
-import "./Products.css";
+import "./Products.scss";
 import { ShoppingCartOutlined } from "@ant-design/icons";
 import { ProductsContext } from "../../context/ProductContext/ProductState";
+
 
 
 const Products = () => {
@@ -46,6 +47,48 @@ const Products = () => {
       })}
     </div>
   );
+
+return(
+  <div className="cont">
+    <div className="search-div">
+        <input className="buscador" type='search' placeholder='Buscar' onChange={handleChange}>
+        </input>
+        <button id ="Button" className="search-btn " onClick={() => buscar(busqueda)}>Search</button>
+        <button id ="Button" className="reset-btn" onClick={() => showAll()}>Show All</button>
+
+        <div className="drop-btns">
+          <div class="dropdown">
+            <button  class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Clothing
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <p class="dropdown-item" onClick={() => filtro(1)}>T-Shirts</p>
+              <p class="dropdown-item" onClick={() => filtro(2)}>Trousers</p>
+              <p class="dropdown-item" onClick={() => filtro(3)}>Hoodies</p>
+              <p class="dropdown-item" onClick={() => filtro(4)}>Accesories</p>
+            </div>
+          </div>
+
+          <div class="dropdown">
+            <button  class="btn btn-outline-dark dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Sort by Price
+            </button>
+            <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <p class="dropdown-item" onClick={() => orderAsc()}>Lowest to Hightest</p>
+              <p class="dropdown-item" onClick={() => orderDesc()}>Highest to Lowest</p>
+            </div>
+          </div>
+        </div>
+
+      </div>
+      <div className="order">
+        {product}
+
+
+      </div>
+  </div>
+  )
+
 };
 
 export default Products;
